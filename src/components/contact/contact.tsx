f3a81fa5a400b3a8e9a0e972d9721ec05895efc3
Zackarias Montell <zackarias@montell.se>
2021-02-07 20:34:54 +0100

    Added profile picture and started with contact layout

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import "./contact.scss";
 import BlinkingCursor from "../../layout/decorative/blinking-cursor";
-import { Link } from "react-router-dom";
+import ProfilePath from "./profile.svg";
 const Contact = () => {
   return (
-    <>
+    <div className="contact">
       <h1 className="color-title">&gt; CONTACT</h1>
-      <div className="row">
-        <span className="code">This page is under construction</span>
-        <BlinkingCursor />
-      </div>
-      <Link to="/test">Go test route</Link>
-    </>
+      <img className="profile-picture" src={ProfilePath} alt="Profile"></img>
+      <div className="row"></div>
+    </div>
   );
 };
 export default Contact;
